79631ab277a450d4dda5afb968980f13b6dabf4d
KeyurPatel <157466601+keyurpatel01@users.noreply.github.com>
2026-03-26 16:39:36 +0530

    Delete first.c

--- first.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h> //stdio : standard input output
-int main(){
-    printf("Hello from Tops..\n");
-    printf("I am Keyur\n");
-    printf("This is not new line!/n")
-    return 0;
-}
